d4417122a5559e954d74c9d51c2a0bc670f5ca53
dlcarper <dcarper@ucmerced.edu>
2020-06-15 17:38:10 -0400

    Added scoring code

--- amPEPpy/amPEP.py
+++ amPEPpy/amPEP.py
@@ -76,6 +76,106 @@ def score(fasta_handle):
            'charge': {1: ['K', 'R'], 2: ['A', 'N', 'C', 'Q', 'G', 'H', 'I', 'L', 'M', 'F', 'P', 'S', 'T', 'W', 'Y', 'V'], 3: ['D', 'E']},
            'secondary': {1: ['E', 'A', 'L', 'M', 'Q', 'K', 'R', 'H'], 2: ['V', 'I', 'Y', 'C', 'W', 'F', 'T'], 3: ['G', 'N', 'P', 'S', 'D']},
            'solvent': {1: ['A', 'L', 'F', 'C', 'G', 'I', 'V', 'W'], 2: ['R', 'K', 'Q', 'E', 'N', 'D'], 3: ['M', 'S', 'P', 'T', 'H', 'Y']}}
+    header=[]
+    groups=[1,2,3]
+    values=[0,25,50,75,100]
+    for property in CTD:
+        for types in groups:
+            for numbers in values:
+                label=""
+                label=label.join("{}.{}.{}".format(property,types,numbers))
+                header.append(label)
+    All_groups=[]
+    Sequence_names=[]
+    for sequences in SeqIO.parse(fasta_handle,"fasta"):
+        sequence_name=sequences.id
+        Sequence_names.append(sequence_name)
+        sequence=str(sequences.seq)
+        sequencelength=len(sequence)
+        Sequence_group=[]
+        for property in CTD:
+            propvalues=""
+            for letter in sequence:
+                if letter in CTD[property][1]:
+                    propvalues+="1"
+                elif letter in CTD[property][2]:
+                    propvalues+="2"
+                else:
+                    propvalues+="3"
+            abpos_1 = [i for i in range(len(propvalues)) if propvalues.startswith("1", i)]
+            abpos_1=[x+1 for x in abpos_1]
+            abpos_1.insert(0,"-")
+            abpos_2 = [i for i in range(len(propvalues)) if propvalues.startswith("2", i)]
+            abpos_2=[x+1 for x in abpos_2]
+            abpos_2.insert(0,"-")
+            abpos_3 = [i for i in range(len(propvalues)) if propvalues.startswith("3", i)]
+            abpos_3=[x+1 for x in abpos_3]
+            abpos_3.insert(0,"-")
+            property_group1_length=propvalues.count("1")
+            if property_group1_length==1:
+                Sequence_group.append((abpos_1[1]/sequencelength)*100)
+                Sequence_group.append((abpos_1[1]/sequencelength)*100)
+                Sequence_group.append((abpos_1[1]/sequencelength)*100)
+                Sequence_group.append((abpos_1[1]/sequencelength)*100)
+                Sequence_group.append((abpos_1[1]/sequencelength)*100)
+            elif property_group1_length==2:
+                Sequence_group.append((abpos_1[1]/sequencelength)*100)
+                Sequence_group.append((abpos_1[1]/sequencelength)*100)
+                Sequence_group.append((abpos_1[round((0.5*property_group1_length)-0.1)]/sequencelength)*100)
+                Sequence_group.append((abpos_1[round((0.75*property_group1_length)-0.1)]/sequencelength)*100)
+                Sequence_group.append((abpos_1[property_group1_length]/sequencelength)*100)
+            else:
+                Sequence_group.append((abpos_1[1]/sequencelength)*100)
+                Sequence_group.append((abpos_1[round((0.25*property_group1_length)-0.1)]/sequencelength)*100)
+                Sequence_group.append((abpos_1[round((0.5*property_group1_length)-0.1)]/sequencelength)*100)
+                Sequence_group.append((abpos_1[round((0.75*property_group1_length)-0.1)]/sequencelength)*100)
+                Sequence_group.append((abpos_1[property_group1_length]/sequencelength)*100)
+
+            property_group2_length=propvalues.count("2")
+            if property_group2_length==1:
+                Sequence_group.append((abpos_2[1]/sequencelength)*100)
+                Sequence_group.append((abpos_2[1]/sequencelength)*100)
+                Sequence_group.append((abpos_2[1]/sequencelength)*100)
+                Sequence_group.append((abpos_2[1]/sequencelength)*100)
+                Sequence_group.append((abpos_2[1]/sequencelength)*100)
+            elif property_group2_length==2:
+                Sequence_group.append((abpos_2[1]/sequencelength)*100)
+                Sequence_group.append((abpos_2[1]/sequencelength)*100)
+                Sequence_group.append((abpos_2[round((0.5*property_group2_length)-0.1)]/sequencelength)*100)
+                Sequence_group.append((abpos_2[round((0.75*property_group2_length)-0.1)]/sequencelength)*100)
+                Sequence_group.append((abpos_2[property_group2_length]/sequencelength)*100)
+            else:
+                Sequence_group.append((abpos_2[1]/sequencelength)*100)
+                Sequence_group.append((abpos_2[round((0.25*property_group2_length)-0.1)]/sequencelength)*100)
+                Sequence_group.append((abpos_2[round((0.5*property_group2_length)-0.1)]/sequencelength)*100)
+                Sequence_group.append((abpos_2[round((0.75*property_group2_length)-0.1)]/sequencelength)*100)
+                Sequence_group.append((abpos_2[property_group2_length]/sequencelength)*100)
+
+            property_group3_length=propvalues.count("3")
+            if property_group3_length==1:
+                Sequence_group.append((abpos_3[1]/sequencelength)*100)
+                Sequence_group.append((abpos_3[1]/sequencelength)*100)
+                Sequence_group.append((abpos_3[1]/sequencelength)*100)
+                Sequence_group.append((abpos_3[1]/sequencelength)*100)
+                Sequence_group.append((abpos_3[1]/sequencelength)*100)
+            elif property_group3_length==2:
+                Sequence_group.append((abpos_3[1]/sequencelength)*100)
+                Sequence_group.append((abpos_3[1]/sequencelength)*100)
+                Sequence_group.append((abpos_3[round((0.5*property_group3_length)-0.1)]/sequencelength)*100)
+                Sequence_group.append((abpos_3[round((0.75*property_group3_length)-0.1)]/sequencelength)*100)
+                Sequence_group.append((abpos_3[property_group3_length]/sequencelength)*100)
+            else:
+                Sequence_group.append((abpos_3[1]/sequencelength)*100)
+                Sequence_group.append((abpos_3[round((0.25*property_group3_length)-0.1)]/sequencelength)*100)
+                Sequence_group.append((abpos_3[round((0.5*property_group3_length)-0.1)]/sequencelength)*100)
+                Sequence_group.append((abpos_3[round((0.75*property_group3_length)-0.1)]/sequencelength)*100)
+                Sequence_group.append((abpos_3[property_group3_length]/sequencelength)*100)
+        All_groups.append(Sequence_group)
+
+    Property_dataframe = pd.DataFrame.from_dict(All_groups)
+    Property_dataframe.columns=header
+    Property_dataframe.index = Sequence_names
+    return(Property_dataframe)
 
 if __name__ == "__main__":
     main()
